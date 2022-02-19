2bb05497ac14ecc26ab57e7910b236844444139b
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-02-19 10:44:55 +0200

    initial commit and lazy loading for english

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'en', loadChildren: () => import('./english/english.module').then(m => m.EnglishModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
