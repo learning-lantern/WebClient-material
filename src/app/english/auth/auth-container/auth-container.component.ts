d59d5fa026bd62395cddc1e80b978b2410ed1b49
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-02-22 16:00:08 +0200

    ok

--- src/app/english/auth/auth-container/auth-container.component.ts
+++ src/app/english/auth/auth-container/auth-container.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-container',
   templateUrl: './auth-container.component.html',
-  styleUrls: ['./auth-container.component.scss']
+  styleUrls: ['./auth-container.component.scss'],
 })
 export class AuthContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    if (localStorage.getItem('token')?.length)
+      this.router.navigate(['/en/welcome']);
   }
 
+  ngOnInit(): void {}
 }
