d11f0f5601fc9470dbc56e8d5b8f2b056ec34b79
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-03-18 15:24:49 +0200

    todo part 1

--- src/app/english/class-list/todo/todo-myday/todo-myday.component.ts
+++ src/app/english/class-list/todo/todo-myday/todo-myday.component.ts
@@ -1,5 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { showDetailService } from '../show-detail.service';
 import { TodoTask } from 'src/app/interface/todo-tasks';
 import { HttpService } from 'src/app/serivces/http.service';
@@ -9,61 +15,126 @@ import { HttpService } from 'src/app/serivces/http.service';
   templateUrl: './todo-myday.component.html',
   styleUrls: ['./todo-myday.component.scss'],
 })
-export class TodoMydayComponent implements OnInit {
-  taskList: TodoTask[] = [];
-  addTaskForm: FormGroup;
+export class TodoMydayComponent implements OnInit, OnDestroy {
+  taskListClient: TodoTask[] = [];
+  addTaskForm: FormGroup = new FormGroup({});
   task!: FormControl;
 
   @Output() showDetail = new EventEmitter<void>();
-  constructor(private detail: showDetailService, private fb: FormBuilder, private https: HttpService) {
-    this.addTaskForm = this.CreatNewTask('task');
+  constructor(
+    private detail: showDetailService,
+    private fb: FormBuilder,
+    private https: HttpService
+  ) {
+    this.CreatNewTask();
   }
-  CreatNewTask(tasks: string): FormGroup {
-    let newTasks = this.fb.group({});
-    newTasks = this.fb.group({
-      task: '',
+  CreatNewTask() {
+    this.addTaskForm = this.fb.group({
+      name: '',
       date: this.fb.group({
         dueDate: '',
         repeat: '0',
       }),
     });
-    return newTasks;
   }
-  getTasks(): FormArray {
-    return <FormArray>this.addTaskForm.get('addNewTask');
-  }
-  addtask() {
-    let newTask = this.addTaskForm.value;
-    this.taskList.push(newTask);
+  // getTasks(): FormArray {
+  //   return <FormArray>this.addTaskForm.get('addNewTask');
+  // }
+  addTask() {
+    let newTask: TodoTask = this.addTaskForm.value;
+    newTask['key'] = this.generateKey();
+    this.taskListClient.push(newTask);
+    this.syncTaskToLoacalStorage();
     let body_ = {
-      Name: this.task.value
+      Name: newTask.name,
+      key: this.generateKey(),
     };
-    this.https.doPost(``, body_, {}).subscribe(
-      (res) => {
-        let result = res as {
-          UserId: {
-            Id: number;
-            Name: string;
-            DueDate: string;
-            Note: string;
-            Completed: boolean;
-            Important: boolean;
-            MyDay: true;
-            Repeated: string;
-          };
-
-        }
-      }
-    )
+    this.https.doPost(``, body_, {}).subscribe((res) => {
+      let result = res as {
+        UserId: {
+          Id: number;
+          Name: string;
+          DueDate: string;
+          Note: string;
+          Completed: boolean;
+          Important: boolean;
+          MyDay: true;
+          Repeated: string;
+          key: string;
+        };
+      };
+    });
   }
 
-  removeTask(item: number) {
-    this.taskList.splice(item, 1);
+  private generateKey(): string {
+    let allowedCharacter = '_=abcdefghijklmnopqrstuv1234567890@#$';
+    let key = '';
+    for (let i = 0; i < 15; i++) {
+      key += allowedCharacter[Math.floor(Math.random() * 37)];
+    }
+    return key;
   }
 
-  ngOnInit(): void { }
+  private syncTaskList(key: string, serverTask: TodoTask): void {
+    let clientTask = this.taskListClient.findIndex((task) => task.key === key);
+    this.taskListClient[clientTask] = serverTask;
+    this.syncTaskToLoacalStorage();
+  }
+  // removeTask(item: number) {
+  //   this.taskList.splice(item, 1);
+  // }
+
+  private syncTaskToLoacalStorage() {
+    localStorage.setItem('cachedTasks', JSON.stringify(this.taskListClient));
+  }
+  ngOnInit(): void {
+    console.log('working');
+    let cache = localStorage.getItem('cachedTasks');
+    if (cache?.length) {
+      this.taskListClient = JSON.parse(cache);
+    } else
+      localStorage.setItem('cachedTasks', JSON.stringify(this.taskListClient));
+    // GET all tasks from the server
+    this.syncFromServer();
+  }
 
   openDetail(task: { name: string; isFavorite: boolean }) {
     this.detail.showDetail.next(task);
   }
+
+  private syncFromServer() {
+    //get request
+    let res: TodoTask[] = [
+      {
+        Id: 10,
+        name: '',
+        isFavorite: false,
+        isMyday: false,
+        isCompleted: false,
+        date: {
+          dueDate: '',
+          repeate: '',
+        },
+        key: '',
+      },
+    ];
+    let cachedTasks = JSON.parse(localStorage.getItem('cachedTasks') || '');
+    if (!cachedTasks.length) {
+      localStorage.setItem('cachedTasks', JSON.stringify(res));
+      this.taskListClient = res;
+    } else {
+      for (let i = 0; i < res.length; i++) {
+        let index = cachedTasks.findIndex((task: TodoTask) => {
+          task.Id == res[i].Id;
+        });
+        if (index) {
+          cachedTasks[index] = res[i];
+          this.taskListClient = cachedTasks;
+          this.syncTaskToLoacalStorage();
+        }
+      }
+    }
+  }
+
+  ngOnDestroy(): void {}
 }
--- src/app/interface/todo-tasks.ts
+++ src/app/interface/todo-tasks.ts
@@ -1,12 +1,12 @@
 export interface TodoTask {
-    id:number;
-    name: string;
-    isFavorite:boolean;
-    isMyday:boolean;
-    isCompleted:boolean;
-    date: {
-      dueDate: string;
-      repeate: string;
-    };
-  
-  }
+  Id: number;
+  name: string;
+  isFavorite: boolean;
+  isMyday: boolean;
+  isCompleted: boolean;
+  date: {
+    dueDate: string;
+    repeate: string;
+  };
+  key: string;
+}
